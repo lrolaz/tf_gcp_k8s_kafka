6b18474a150f3df1d4997c151d1f1189606369ac
Laurent Rolaz <lrolaz@pictet.com>
2018-10-01 15:55:41 +0200

    add token

--- vault.tf
+++ vault.tf
@@ -117,6 +117,29 @@ resource "google_compute_address" "vault" {
   depends_on = ["google_project_service.service"]
 }
 
+# Download the encrypted root token to disk
+data "google_storage_object_signed_url" "root-token" {
+  bucket = "${google_storage_bucket.vault.name}"
+  path   = "root-token.enc"
+
+  credentials = "${base64decode(google_service_account_key.vault.private_key)}"
+}
+
+# Download the encrypted file
+data "http" "root-token" {
+  url = "${data.google_storage_object_signed_url.root-token.signed_url}"
+}
+
+# Decrypt the secret
+data "google_kms_secret" "root-token" {
+  crypto_key = "${google_kms_crypto_key.vault-init.id}"
+  ciphertext = "${data.http.root-token.body}"
+}
+
+output "token" {
+  value = "${data.google_kms_secret.root-token.plaintext}"
+}
+
 output "address" {
   value = "${google_compute_address.vault.address}"
 }
